3a726f811e37eba2cd15938ed36a4ec4c1fb1f1a
yoannPaquette <43345830+yoannPaquette@users.noreply.github.com>
2018-09-28 23:10:27 -0400

    Modification + ajout commentaires dans scripts système d'évènement

--- Assets/Scripts/SystemeEventsLib.cs
+++ Assets/Scripts/SystemeEventsLib.cs
@@ -1,18 +1,20 @@
 ﻿namespace UnityEngine
 {
+    //Namespace personaliser pour la classe d'information du système d'évènement
     namespace SystemeEventsLib
     {
+        //Liste des différents évènements
         public enum NomEvent { MortEvent, AttaqueEvent, DegatEvent, ScoreUpdateEvent}
+        //Signature des fonctions accepté dans le dictionaire d'évènements
         public delegate void Action<in InfoEvent>(InfoEvent infoEvent);
 
+        //Classe d'informations des évènements
         public class InfoEvent
         {
-            public int Id { get; set; }
-            public GameObject Cible { get; set; }
-            public int Degat { get; set; }
-            public int Vie { get; set; }
-            public GameObject BarreVie { get; set; }
+            public int Id { get; set; } //Identification de l'objet
+            public GameObject Cible { get; set; } //Cible qui doit être affectée
 
+            //Contructeur par défaut de la classe
             public InfoEvent(int id = 10000, GameObject cible = null)
             {
                 this.Id = id;
