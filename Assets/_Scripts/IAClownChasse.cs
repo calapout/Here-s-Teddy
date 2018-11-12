cc8a60f1e8e398913859f44a5970a0628b5cb50e
catherine Beaudoin-Rheault <catherinebr24@gmail.com>
2018-11-12 12:58:10 -0500

    Debut clown

--- Assets/_Scripts/IAClownChasse.cs
+++ Assets/_Scripts/IAClownChasse.cs
@@ -1,18 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 
-public class IAVoitureChasse : StateMachineBehaviour {
+public class IAClownChasse : StateMachineBehaviour {
 
     private GameObject _Teddy;
+
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-    }
+    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
