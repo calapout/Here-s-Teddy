4891b903f5889c9afa2b183489eaba25bc42b258
yoannPaquette <43345830+yoannPaquette@users.noreply.github.com>
2018-12-19 23:35:01 -0500

    Explosion des bombes intégré

---
+++ Assets/_Scripts/InstanciationExplosion.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class InstanciationExplosion : MonoBehaviour {
+
+    Vector3 position;
+
+    public void Boom()
+    {
+        position = transform.position;
+        Instantiate(Resources.Load("Explosion_Res") as GameObject, position, new Quaternion());
+    }
+}
