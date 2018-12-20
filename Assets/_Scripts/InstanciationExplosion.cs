68637ee01e589478356e022762c693b3fe87983c
catherine Beaudoin-Rheault <43348739+alia600@users.noreply.github.com>
2018-12-20 00:31:51 -0500

    commentaire du script de la bombe

--- Assets/_Scripts/InstanciationExplosion.cs
+++ Assets/_Scripts/InstanciationExplosion.cs
@@ -1,10 +1,18 @@
-﻿using UnityEngine;
+﻿
+/**
+ * Gére l'instanciation de la bombe
+ * @author Catherine Beaudoin-Rheault et Yoann Paquette
+ * @version 2018-12-20
+ * */
+using UnityEngine;
 
 public class InstanciationExplosion : MonoBehaviour {
 
     AudioSource audioSource;//variable de l'audiosource
-    Vector3 position;
+    Vector3 position;//postion de l'explosion
     
+
+    /*Fonction qui permet d'activé l'explosion et de jouer son son*/
     public void Boom()
     {
         audioSource = GetComponent<AudioSource>();
