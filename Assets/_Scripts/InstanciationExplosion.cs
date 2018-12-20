830ade730e1542d568a20e2692500990bc5e6f60
catherine Beaudoin-Rheault <43348739+alia600@users.noreply.github.com>
2018-12-20 00:25:37 -0500

    son explosion sur les bombes

--- Assets/_Scripts/InstanciationExplosion.cs
+++ Assets/_Scripts/InstanciationExplosion.cs
@@ -2,10 +2,13 @@
 
 public class InstanciationExplosion : MonoBehaviour {
 
+    AudioSource audioSource;//variable de l'audiosource
     Vector3 position;
-
+    
     public void Boom()
     {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.enabled = true;
         position = transform.position;
         Instantiate(Resources.Load("Explosion_Res") as GameObject, position, new Quaternion());
     }
